72916f588492c569a88146ece87e7d876f7fa303
Sibi Prabakaran <sibi@psibi.in>
2021-03-28 22:38:03 +0530

    More code

--- src/weather.rs
+++ src/weather.rs
@@ -1,5 +1,6 @@
 use nom::branch::alt;
-use nom::bytes::streaming::{tag_no_case, take_till};
+use nom::bytes::streaming::{tag_no_case, take, take_till};
+use nom::Err::Error;
 use nom::IResult;
 use std::convert::TryFrom;
 
@@ -59,7 +60,7 @@ impl TryFrom<&str> for Station {
     }
 }
 
-pub fn parse_missing_station_name(i: &str) -> IResult<&str, Option<Station>> {
+pub fn parse_station(i: &str) -> IResult<&str, Option<Station>> {
     let result = alt((
         tag_no_case("Station name not available"),
         take_till(|c| c == '\n'),
@@ -76,22 +77,61 @@ pub fn parse_missing_station_name(i: &str) -> IResult<&str, Option<Station>> {
     }
 }
 
+#[derive(PartialEq, Debug)]
+pub struct WeatherTime {
+    pub year: String,
+    pub month: String,
+    pub day: String,
+    // pub year: u16,
+    // pub month: u8,
+    // pub day: u8,
+    pub time: String,
+}
+
+pub fn parse_time(i: &str) -> IResult<&str, Option<WeatherTime>> {
+    // Parsers a sample string like this
+    // Mar 28, 2021 - 04:00 AM EDT / 2021.03.28 0800 UTC
+    let (i, _) = take_till(|c| c == '/')(i)?;
+    let (i, _) = take(1usize)(i)?;
+    match i.trim().split('.').collect::<Vec<&str>>()[..] {
+        [ref y, ref m, ref d] => {
+            let mut time = d.split(' ').collect::<Vec<&str>>();
+            let day = time.remove(0);
+            return Ok((
+                i,
+                Some(WeatherTime {
+                    year: y.to_string(),
+                    month: m.to_string(),
+                    day: day.to_string(),
+                    time: time.join(" "),
+                }),
+            ));
+        }
+        _ => return Ok((i, None)),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn test_station() {
-        assert_eq!(
-            parse_missing_station_name("Station name not available"),
-            Ok(("", None))
-        );
+        assert_eq!(parse_station("Station name not available"), Ok(("", None)));
         let station = Station {
             place: "Qingdao".to_string(),
             country: "China".to_string(),
         };
         assert_eq!(
-            parse_missing_station_name("Qingdao, China (ZSQD) 36-04N 120-20E 77M\n"),
+            parse_station("Qingdao, China (ZSQD) 36-04N 120-20E 77M\n"),
             Ok(("\n", Some(station)))
+        );
+    }
+
+    #[test]
+    fn test_time() {
+        assert_eq!(
+            parse_time("Mar 28, 2021 - 04:00 AM EDT / 2021.03.28 0800 UTC"),
+            Ok(("", None))
         );
     }
 }
